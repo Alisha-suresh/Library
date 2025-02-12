84f339abe8fa1227198b5db959aced885d2297ab
Alisha-suresh <thoughtfilterapp@gmail.com>
2025-02-12 23:32:54 +0000

    landing page

--- mini-library/src/app/app.routes.ts
+++ mini-library/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { BookListComponent } from './components/book-list/book-list.component';
-import { BookDetailComponent } from './components/book-detail/book-detail.component';
+import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 
-export const routes: Routes = [];
 export const appRoutes: Routes = [
-    { path: '', component: BookListComponent },
-    { path: 'book/:id', component: BookDetailComponent },
+    { path: '', component: HomeComponent },
+    { path: 'books', component: BookListComponent },
     { path: 'search', component: SearchComponent },
+    { path: '**', redirectTo: '' }
 ];
--- mini-library/src/app/components/home/home.component.ts
+++ mini-library/src/app/components/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MatButtonModule, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
