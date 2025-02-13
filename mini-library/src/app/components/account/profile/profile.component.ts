9ece91050e8e9a40609714e177f290e03683c7f1
Alisha-suresh <thoughtfilterapp@gmail.com>
2025-02-13 03:50:06 +0000

    account done

--- mini-library/src/app/components/account/profile/profile.component.ts
+++ mini-library/src/app/components/account/profile/profile.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../../interfaces/user.interface';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,
+    MatCardModule,
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-
+  user: User = {
+    name: 'John Doe',
+    email: 'john@example.com',
+    phone: '+1 234 567 8900'
+  };
 }
--- mini-library/src/app/components/account/settings/settings.component.ts
+++ mini-library/src/app/components/account/settings/settings.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,
+    MatCardModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatDialogModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
 export class SettingsComponent {
+  isDarkMode = false;
 
+  constructor(private dialog: MatDialog) { }
+
+  toggleDarkMode(event: any) {
+    this.isDarkMode = event.checked;
+    // Implement dark mode logic
+  }
+
+  signOut() {
+    // Implement sign out logic
+  }
+
+  deleteAccount() {
+
+  }
 }
